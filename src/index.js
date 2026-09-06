b6945af224f1b3a4a685d40e7bfa6554116efda9
ajs <140010746+aboodJS@users.noreply.github.com>
2026-09-06 16:41:51 +0300

    added some boilerplate code

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(3000, () => {
+  console.log(`server running on: ${process.env.SERVER_URI}`);
+});
